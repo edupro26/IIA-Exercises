bbf4fd8a44d56ca49ad5496f53dae22de858c512
AlexPinto14 <apinto141414@gmail.com>
2023-10-03 18:37:34 +0100

    Implemented parse_grid_and_T
    to make the code more modular and not repeat code on further methods

--- AC1/MedoTotal.py
+++ AC1/MedoTotal.py
@@ -1,27 +1,4 @@
-from searchPlus import *
-
-parametros = "T=26\nM=6\nP=10"
-linha1 = "= = = = = = = = = =\n"
-linha2 = "= @ . * . . * . . =\n"
-linha3 = "= . = = = = = = . =\n"
-linha4 = "= . = F . . . . . =\n"
-linha5 = "= . = . . . . . . =\n"
-linha6 = "= . = . . . . . . =\n"
-linha7 = "= . = . . . . . . =\n"
-linha8 = "= * . . . . . . . =\n"
-linha9 = "= . . . . . . . . =\n"
-linha10 = "= = = = = = = = = =\n"
-grelha = linha1 + linha2 + linha3 + linha4 + linha5 + linha6 + linha7 + linha8 + linha9 + linha10
-mundoStandard = parametros + "\n" + grelha
-
-
-class MedoTotal(Problem):
-
-    def __init__(self, situacaoInicial=mundoStandard):
-        super().__init__(initial=situacaoInicial)
-        self.situacaoInicial = situacaoInicial
-
-    from searchPlus import Problem
+from searchPlus import Problem
 
 parametros="T=26\nM=6\nP=10"
 linha1= "= = = = = = = = = =\n"
@@ -44,12 +21,7 @@ class MedoTotal(Problem):
         self.situacaoInicial = situacaoInicial
    
     def actions(self, state):
-        state_lines = state.split('\n')
-        parameters = state_lines[0].split('\n')
-        T, M, P = map(int, parameters[0].split('=')[1].split())
-
-        #Gets the grid of the state
-        grid = [list(line) for line in state_lines[1:]]
+        grid, T = self.parse_grid_and_t(state)
 
         #Pacman's position
         pacman_x, pacman_y = None, None
@@ -91,9 +63,20 @@ class MedoTotal(Problem):
                 else:
                     return True
         return False
+    
+    def parse_grid_and_t(self, state):
+        state_lines = state.split('\n')
+        parameters = state_lines[0].split('\n')
+        T, M, P = map(int, parameters[0].split('=')[1].split())
+
+        #Gets the grid of the state
+        grid = [list(line) for line in state_lines[1:]]
+
+        return grid, T
         
     def result(self, state, action):
         pass
+
     
     def path_cost(self, c, state1,action,next_state):
         pass
