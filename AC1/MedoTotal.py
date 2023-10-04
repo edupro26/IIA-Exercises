09c63209cadcfd02892ed1690c6b4c1fd87b8c39
Eduardo Proença <edupro26@gmail.com>
2023-10-04 12:40:47 +0100

    Auxiliar methods to the bottom of MedoTotal

--- AC1/MedoTotal.py
+++ AC1/MedoTotal.py
@@ -22,7 +22,7 @@ class MedoTotal(Problem):
         self.situacaoInicial = situacaoInicial
 
     def actions(self, state):
-        grid, T = self.parse_grid_and_t(state)
+        grid, t = self.parse_grid_and_t(state)
 
         # Pacman's position
         pacman_x, pacman_y = self.find_pacman_position(grid)
@@ -33,10 +33,46 @@ class MedoTotal(Problem):
         for direction in possible_directions:
             new_x, new_y = self.get_new_position(pacman_x, pacman_y, direction)
 
-            if self.is_valid_position(new_x, new_y, grid, T):
+            if self.is_valid_position(new_x, new_y, grid, t):
                 valid_actions.append(direction)
 
         return valid_actions
+
+    def result(self, state, action):
+        new_state = state
+
+        grid, t = self.parse_grid_and_t(state)
+        pacman_x, pacman_y = self.find_pacman_position(grid)
+
+        new_x, new_y = self.get_new_position(pacman_x, pacman_y, action)
+        if self.is_valid_position(new_x, new_y, grid, t):
+            grid[pacman_x][pacman_y] = "."
+            grid[new_x][new_y] = "@"
+
+            # Remakes the new state
+            new_state_lines = [f"t={t}\n"]
+            new_state_lines.extend(["".join(line) + "\n" for line in grid])
+            new_state = "".join(new_state_lines)
+
+        return new_state
+
+    def path_cost(self, c, state1, action, next_state):
+        # TODO
+        pass
+
+    def executa(self, state, actions):
+        """Partindo de state, executa a sequência (lista) de acções (em actions) e devolve o último estado"""
+        nstate = state
+        for a in actions:
+            nstate = p.result(nstate, a)
+        return nstate
+
+    def display(self, state):
+        """Devolve a grelha em modo txt"""
+        return state[len(parametros) + 1:len(state)]
+
+
+    # Auxiliar functions of MedoTotal class
 
     def is_valid_position(self, x, y, grid, T):
         if 0 <= x < len(grid) and 0 <= y < len(grid[0]):
@@ -52,12 +88,12 @@ class MedoTotal(Problem):
     def parse_grid_and_t(self, state):
         state_lines = state.split('\n')
         parameters = state_lines[0].split('\n')
-        T, M, P = map(int, parameters[0].split('=')[1].split())
+        t, m, p = map(int, parameters[0].split('=')[1].split())
 
         # Gets the grid of the state
         grid = [list(line) for line in state_lines[1:]]
 
-        return grid, T
+        return grid, t
 
     def find_pacman_position(self, grid):
         for x in range(len(grid)):
@@ -79,39 +115,3 @@ class MedoTotal(Problem):
             new_y -= 1
 
         return new_x, new_y
-
-    def result(self, state, action):
-        new_state = state
-
-        grid, T = self.parse_grid_and_t(state)
-        pacman_x, pacman_y = self.find_pacman_position(grid)
-
-        new_x, new_y = self.get_new_position(pacman_x, pacman_y, action)
-        if self.is_valid_position(new_x, new_y, grid, T):
-            grid[pacman_x][pacman_y] = "."
-            grid[new_x][new_y] = "@"
-
-            # Remakes the new state
-            new_state_lines = [f"T={T}\n"]
-            new_state_lines.extend(["".join(line) + "\n" for line in grid])
-            new_state = "".join(new_state_lines)
-
-        return new_state
-
-    def path_cost(self, c, state1, action, next_state):
-        # TODO
-        pass
-
-    def executa(self, state, actions):
-        """Partindo de state, executa a sequência (lista) de acções (em actions) e devolve o último estado"""
-        nstate = state
-        for a in actions:
-            nstate = p.result(nstate, a)
-        return nstate
-
-    def display(self, state):
-        """Devolve a grelha em modo txt"""
-        return state[len(parametros) + 1:len(state)]
-
-g = MedoTotal()
-print(g.actions(g.initial))
