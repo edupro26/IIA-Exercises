736efdfffb0f6b4e414da771e8452e2d873def4c
Eduardo Proença <edupro26@gmail.com>
2023-10-04 12:06:13 +0100

    Removed MedoTotal class from jupiter file

--- AC1/MedoTotal.py
+++ AC1/MedoTotal.py
@@ -1,29 +1,30 @@
-from searchPlus import Problem
+from searchPlus import *
 
-parametros="T=26\nM=6\nP=10"
-linha1= "= = = = = = = = = =\n"
-linha2= "= @ . * . . * . . =\n"
-linha3= "= . = = = = = = . =\n"
-linha4= "= . = F . . . . . =\n"
-linha5= "= . = . . . . . . =\n"
-linha6= "= . = . . . . . . =\n"
-linha7= "= . = . . . . . . =\n"
-linha8= "= * . . . . . . . =\n"
-linha9= "= . . . . . . . . =\n"
-linha10="= = = = = = = = = =\n"
-grelha=linha1+linha2+linha3+linha4+linha5+linha6+linha7+linha8+linha9+linha10
-mundoStandard=parametros + "\n" + grelha
+parametros = "T=26\nM=6\nP=10"
+linha1 = "= = = = = = = = = =\n"
+linha2 = "= @ . * . . * . . =\n"
+linha3 = "= . = = = = = = . =\n"
+linha4 = "= . = F . . . . . =\n"
+linha5 = "= . = . . . . . . =\n"
+linha6 = "= . = . . . . . . =\n"
+linha7 = "= . = . . . . . . =\n"
+linha8 = "= * . . . . . . . =\n"
+linha9 = "= . . . . . . . . =\n"
+linha10 = "= = = = = = = = = =\n"
+grelha = linha1 + linha2 + linha3 + linha4 + linha5 + linha6 + linha7 + linha8 + linha9 + linha10
+mundoStandard = parametros + "\n" + grelha
+
 
 class MedoTotal(Problem):
-    
+
     def __init__(self, situacaoInicial=mundoStandard):
-        super().__init__(initial = situacaoInicial)
+        super().__init__(initial=situacaoInicial)
         self.situacaoInicial = situacaoInicial
-   
+
     def actions(self, state):
         grid, T = self.parse_grid_and_t(state)
 
-        #Pacman's position
+        # Pacman's position
         pacman_x, pacman_y = self.find_pacman_position(grid)
 
         possible_directions = ["N", "S", "E", "W"]
@@ -31,13 +32,12 @@ class MedoTotal(Problem):
 
         for direction in possible_directions:
             new_x, new_y = self.get_new_position(pacman_x, pacman_y, direction)
-            
+
             if self.is_valid_position(new_x, new_y, grid, T):
                 valid_actions.append(direction)
-    
+
         return valid_actions
-            
-    
+
     def is_valid_position(self, x, y, grid, T):
         if 0 <= x < len(grid) and 0 <= y < len(grid[0]):
             if grid[x][y] != "=":
@@ -48,24 +48,24 @@ class MedoTotal(Problem):
                 else:
                     return True
         return False
-    
+
     def parse_grid_and_t(self, state):
         state_lines = state.split('\n')
         parameters = state_lines[0].split('\n')
         T, M, P = map(int, parameters[0].split('=')[1].split())
 
-        #Gets the grid of the state
+        # Gets the grid of the state
         grid = [list(line) for line in state_lines[1:]]
 
         return grid, T
-    
+
     def find_pacman_position(self, grid):
         for x in range(len(grid)):
             for y in range(len(grid[x])):
                 if grid[x][y] == "@":
                     return x, y
         return None, None
-    
+
     def get_new_position(self, x, y, direction):
         new_x, new_y = x, y
 
@@ -79,7 +79,7 @@ class MedoTotal(Problem):
             new_y -= 1
 
         return new_x, new_y
-        
+
     def result(self, state, action):
         new_state = state
 
@@ -95,7 +95,7 @@ class MedoTotal(Problem):
             new_state_lines = [f"T={T}\n"]
             new_state_lines.extend(["".join(line) + "\n" for line in grid])
             new_state = "".join(new_state_lines)
-        
+
         return new_state
 
     def path_cost(self, c, state1, action, next_state):
@@ -111,4 +111,7 @@ class MedoTotal(Problem):
 
     def display(self, state):
         """Devolve a grelha em modo txt"""
-        return state[len(parametros)+1:len(state)]
+        return state[len(parametros) + 1:len(state)]
+
+g = MedoTotal()
+print(g.actions(g.initial))
