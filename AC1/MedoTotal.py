2fd5d81b513af5ebe3de925fed052cefb12a38ab
edupro26 <edupro26@gmail.com>
2023-10-05 18:20:24 +0100

    Code organization (all auxiliar method are now static)

--- AC1/MedoTotal.py
+++ AC1/MedoTotal.py
@@ -1,3 +1,11 @@
+"""
+    Avaliação Contínua 1 - MedoTotal
+
+    Grupo 26:
+    Eduardo Proença 57551
+    Alexandre Pinto 55958
+"""
+
 from searchPlus import *
 
 parametros = "T=26\nM=6\nP=10"
@@ -15,6 +23,10 @@ grelha = linha1 + linha2 + linha3 + linha4 + linha5 + linha6 + linha7 + linha8 +
 mundoStandard = parametros + "\n" + grelha
 
 
+# ___________________________________________________________________________________
+# Class MedoTotal
+
+
 class MedoTotal(Problem):
 
     def __init__(self, situacaoInicial=mundoStandard):
@@ -24,12 +36,12 @@ class MedoTotal(Problem):
     def actions(self, state):
         t = int(parametros.split('\n')[0][2:])
         m = int(parametros.split('\n')[1][2:])
-        grid = self.parse_grid(state)
+        grid = parse_grid(state)
 
         # Pacman's position
-        pacman_x, pacman_y = self.find_pacman_position(grid)
+        pacman_x, pacman_y = find_pacman_position(grid)
 
-        min_d_to_pill = self.min_distance_to_pill(pacman_x, pacman_y, grid)
+        min_d_to_pill = min_distance_to_pill(pacman_x, pacman_y, grid)
 
         if min_d_to_pill > int(parametros.split('\n')[2][2:]) - 1 and t > m:
             return []
@@ -41,9 +53,9 @@ class MedoTotal(Problem):
         valid_actions = []
 
         for direction in possible_directions:
-            new_x, new_y = self.get_new_position(pacman_x, pacman_y, direction)
+            new_x, new_y = get_new_position(pacman_x, pacman_y, direction)
 
-            if self.is_valid_position(new_x, new_y, grid, t):
+            if is_valid_position(new_x, new_y, grid, t):
                 valid_actions.append(direction)
 
         return valid_actions
@@ -55,9 +67,9 @@ class MedoTotal(Problem):
         p = int(state_lines[2][2:])
 
         new_state = state
-        grid = self.parse_grid(state)
-        pacman_x, pacman_y = self.find_pacman_position(grid)
-        new_x, new_y = self.get_new_position(pacman_x, pacman_y, action)
+        grid = parse_grid(state)
+        pacman_x, pacman_y = find_pacman_position(grid)
+        new_x, new_y = get_new_position(pacman_x, pacman_y, action)
 
         if grid[new_x][new_y] == "*":
             m = p
@@ -99,74 +111,79 @@ class MedoTotal(Problem):
         return state[start:end]
 
 
-    # Auxiliar functions of MedoTotal class
+# ___________________________________________________________________________________
+# Auxiliar functions of MedoTotal class
 
-    def is_valid_position(self, x, y, grid, T):
-        if 0 <= x < len(grid) and 0 <= y < len(grid[0]):
-            if grid[x][y] != "=" and grid[x][y] != "F":
-                if grid[x][y] != "." and grid[x][y] != "*":
-                    visited_count = int(grid[x][y])
-                    if visited_count < T:
-                        return True
-                else:
+
+def is_valid_position(x, y, grid, T):
+    if 0 <= x < len(grid) and 0 <= y < len(grid[0]):
+        if grid[x][y] != "=" and grid[x][y] != "F":
+            if grid[x][y] != "." and grid[x][y] != "*":
+                visited_count = int(grid[x][y])
+                if visited_count < T:
                     return True
-        return False
+            else:
+                return True
+    return False
 
 
-    def parse_grid(self, state):
-        state_lines = state.split('\n')
+def parse_grid(state):
+    state_lines = state.split('\n')
 
-        for i in range(len(state_lines)):
-            state_lines[i] = state_lines[i].replace(' ', '')
+    for i in range(len(state_lines)):
+        state_lines[i] = state_lines[i].replace(' ', '')
 
-        # Gets the grid of the state
-        grid = [list(line) for line in state_lines[3:]]
+    # Gets the grid of the state
+    grid = [list(line) for line in state_lines[3:]]
 
-        return grid
+    return grid
 
-    def find_pacman_position(self, grid):
-        for x in range(len(grid)):
-            for y in range(len(grid[x])):
-                if grid[x][y] == "@":
-                    return x, y
-        return None, None
 
-    def get_new_position(self, x, y, direction):
-        new_x, new_y = x, y
+def find_pacman_position(grid):
+    for x in range(len(grid)):
+        for y in range(len(grid[x])):
+            if grid[x][y] == "@":
+                return x, y
+    return None, None
 
-        if direction == "N":
-            new_x -= 1
-        elif direction == "S":
-            new_x += 1
-        elif direction == "E":
-            new_y += 1
-        elif direction == "W":
-            new_y -= 1
 
-        return new_x, new_y
-    
-    def min_distance_to_pill(self, pacman_x, pacman_y, grid):
-        queue = [(pacman_x, pacman_y, 0)]
+def get_new_position(x, y, direction):
+    new_x, new_y = x, y
 
-        visited = set()
-        visited.add((pacman_x, pacman_y))
+    if direction == "N":
+        new_x -= 1
+    elif direction == "S":
+        new_x += 1
+    elif direction == "E":
+        new_y += 1
+    elif direction == "W":
+        new_y -= 1
 
-        while queue:
-            x, y, distance = queue.pop(0)
+    return new_x, new_y
 
-            if grid[x][y] == "*":
-                return distance
 
-            for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-                new_x, new_y = x + dx, y + dy
+def min_distance_to_pill(pacman_x, pacman_y, grid):
+    queue = [(pacman_x, pacman_y, 0)]
 
-                if (
-                    0 <= new_x < len(grid)
-                    and 0 <= new_y < len(grid[0])
-                    and grid[new_x][new_y] != "="
-                    and (new_x, new_y) not in visited
-                ):
-                    queue.append((new_x, new_y, distance + 1))
-                    visited.add((new_x, new_y))
+    visited = set()
+    visited.add((pacman_x, pacman_y))
 
-        return float('inf')
+    while queue:
+        x, y, distance = queue.pop(0)
+
+        if grid[x][y] == "*":
+            return distance
+
+        for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            new_x, new_y = x + dx, y + dy
+
+            if (
+                0 <= new_x < len(grid)
+                and 0 <= new_y < len(grid[0])
+                and grid[new_x][new_y] != "="
+                and (new_x, new_y) not in visited
+            ):
+                queue.append((new_x, new_y, distance + 1))
+                visited.add((new_x, new_y))
+
+    return float('inf')
