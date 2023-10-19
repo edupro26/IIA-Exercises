ea6edd246838d100ab7b5675683a6699f1f4c776
edupro26 <edupro26@gmail.com>
2023-10-19 14:53:33 +0100

    Visited count fixed

--- AC2/tree_search.py
+++ AC2/tree_search.py
@@ -2,25 +2,22 @@
 from MedoTotal import *
 
 def depth_first_tree_search_all_count(problem,optimal=False,verbose=False):
-    visited, final_states = [], []
-    reached_goal = False
+    final_states = []
+    visitados = 0
     frontier = Stack()
     frontier.append(Node(problem.initial))
 
     while frontier:
         node = frontier.pop()
         print(node.state)
-        visited.append(node)
+        visitados += 1
 
         if verbose:
             print(problem.display(node.state))
 
         if problem.goal_test(node.state):
-            reached_goal = True
             final_states.append(node)
-            #return (node, None, visited, final_states)
-
-        if not reached_goal:
+        else:
             frontier.extend(node.expand(problem))
 
-    return (final_states[0], None, len(visited), len(final_states))
+    return (final_states[0], None, visitados, len(final_states))
