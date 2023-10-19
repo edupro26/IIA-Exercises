31f201c5baf739859083aa9974c4a78df98d56b9
edupro26 <edupro26@gmail.com>
2023-10-20 00:50:34 +0100

    Updated tree_search function

--- AC2/tree_search.py
+++ AC2/tree_search.py
@@ -1,90 +1,54 @@
 
 from MedoTotal import *
 
-def depth_first_tree_search_all_count(problem, optimal=False, verbose=False):
-    max_mem, visitados, final_states = 0, 0, []
-    frontier = Stack()
-    frontier.append(Node(problem.initial))
-    best_solution = None
 
+def depth_first_tree_search_all_count(problem, optimal=False, verbose=False):
+    max_mem, visited, final_states = 0, 0, []
+    frontier = [Node(problem.initial)]
     while frontier:
         max_mem = max(len(frontier), max_mem)
         node = frontier.pop()
-        visitados += 1
+        visited += 1
         if verbose:
-            display_expand(node, problem, best_solution)
+            display_node(node, problem, None)
 
         expanded = node.expand(problem)
-        if node.state.tempo != 1:
-            expanded.reverse()
+        if expanded:
+            if problem.goal_test(expanded[0].state):
+                solution = expanded[0]
+                solution, visited, final_states = search_solutions(problem, expanded, solution, visited,
+                                                                   optimal, verbose)
+                return (solution, max_mem, visited, len(final_states))
 
-        for node in range(len(expanded)):
-            if problem.goal_test(expanded[node].state):
-                if best_solution is None or expanded[node].path_cost < best_solution.path_cost:
-                    best_solution = expanded[node]
-
-                if optimal:
-                    if best_solution not in final_states:
-                        final_states.append(best_solution)
-                        visitados += 1
-                        if verbose:
-                            display_expand(expanded[node], problem, best_solution)
-                else:
-                    final_states.append(expanded[node])
-                    visitados += 1
-                    if verbose:
-                        display_expand(expanded[node], problem, best_solution)
-            else:
-                frontier.append(expanded[node])
-
-    return (best_solution, max_mem, visitados, len(final_states))
+        expanded.reverse()
+        frontier.extend(expanded)
+    return (None, max_mem, visited, len(final_states))
 
 
-def display_expand(node, problem, best_solution):
+def search_solutions(problem, expanded, solution, visited, optimal, verbose):
+    final_states = []
+    for node in expanded:
+        if node.path_cost < solution.path_cost:
+            solution = node
+        if not optimal:
+            final_states.append(node)
+            visited += 1
+            if verbose:
+                display_node(node, problem, solution)
+        elif solution not in final_states:
+            final_states.append(node)
+            visited += 1
+            if verbose:
+                display_node(node, problem, solution)
+    return solution, visited, final_states
+
+
+def display_node(node, problem, solution):
     print('---------------------\n')
     print(problem.display(node.state))
-    if node.state.tempo == 0:
+    if problem.goal_test(node.state):
         print(f"GGGGooooooallllll --------- com o custo: {node.path_cost}")
-        if node == best_solution:
+        if node == solution:
             print(f"Di best goal até agora")
     else:
         print(f"Custo: {node.path_cost}")
-
-
-
-
-#def depth_first_tree_search_all_count(problem,optimal=False,verbose=False):
-#    max_mem, visitados, final_states = 0, 0, []
-#    frontier = Stack()
-#    frontier.append(Node(problem.initial))
-#
-#    while frontier:
-#        max_mem = max(len(frontier), max_mem)
-#        node = frontier.pop()
-#        visitados += 1
-#        if verbose:
-#            display_expand(node, problem)
-#
-#        if problem.goal_test(node.state):
-#            final_states.append(node)
-#        else:
-#            expanded = node.expand(problem)
-#            expanded.reverse()
-#            if optimal and node.state.tempo == 1:
-#                optimize(expanded, frontier)
-#            else:
-#                if node.state.tempo > 1:
-#                    frontier.extend(expanded)
-#
-#    return (get_best_solution(final_states), max_mem, visitados, len(final_states))
-
-
-#def optimize(expanded, frontier):
-#    costs = []
-#    i = 0
-#    for node in expanded:
-#        if node.path_cost in costs:
-#            expanded.pop(i)
-#        costs.append(node.path_cost)
-#        i += 1
-#    frontier.extend(expanded)
