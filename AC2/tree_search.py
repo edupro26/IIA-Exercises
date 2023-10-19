501154a5858f01e09d5749622c1e6103fd752e32
edupro26 <edupro26@gmail.com>
2023-10-19 17:12:18 +0100

    Optimize and display implemented

--- AC2/tree_search.py
+++ AC2/tree_search.py
@@ -2,47 +2,44 @@
 from MedoTotal import *
 
 def depth_first_tree_search_all_count(problem,optimal=False,verbose=False):
-    final_states = []
-    visitados, max_mem = 0, 0
+    max_mem, visitados, final_states = 0, 0, []
     frontier = Stack()
     frontier.append(Node(problem.initial))
 
     while frontier:
         node = frontier.pop()
         visitados += 1
-
         if verbose:
-            print(problem.display(node.state))
+            display_expand(node, problem)
 
         if problem.goal_test(node.state):
             final_states.append(node)
         else:
-            if len(frontier) > max_mem:
-                max_mem = len(frontier)
-
-            frontier.extend(node.expand(problem))
+            max_mem = max(len(frontier), max_mem)
+            if optimal and node.state.tempo == 1:
+                frontier.extend(optimize(node, problem))
+            else:
+                frontier.extend(node.expand(problem))
 
     return (final_states[0], max_mem, visitados, len(final_states))
 
 
-parametros="T=6\nM=4\nP=10"
-linha1= "= = = = = =\n"
-linha2= "= . @ F * =\n"
-linha3= "= . . . . =\n"
-linha4= "= . = . . =\n"
-linha5= "= . = . . =\n"
-linha6= "= = = = = =\n"
-grelha=linha1+linha2+linha3+linha4+linha5+linha6
-mundoStandardx=parametros + "\n" + grelha
-gx=MedoTotal(mundoStandardx)
+def display_expand(node, problem):
+    print('---------------------\n')
+    print(problem.display(node.state))
+    if node.state.tempo == 0:
+        print(f"GGGGooooooallllll --------- com o custo: {node.path_cost}")
+    else:
+        print(f"Custo: {node.path_cost}")
 
-resultado,max_mem,visitados,finais = depth_first_tree_search_all_count(gx)
-print('*'*20)
-if resultado:
-    print("Solução Prof-total (árvore) com custo " + str(resultado.path_cost)+":")
-    print(resultado.solution())
-else:
-    print('\nSem Solução')
-print('Visitados=',visitados)
-print('Dimensão máxima da memória',max_mem)
-print('Estados finais:',finais)
+
+def optimize(node, problem):
+    expanded = node.expand(problem)
+    costs = []
+    i = 0
+    for node in expanded:
+        if node.path_cost in costs:
+            expanded.pop(i)
+        costs.append(node.path_cost)
+        i += 1
+    return expanded
