5da63e89d96c4e1656c8f56a373e3d57d8b6d7b4
edupro26 <edupro26@gmail.com>
2023-10-20 17:40:09 +0100

    All tests passing

--- AC2/tree_search.py
+++ AC2/tree_search.py
@@ -5,6 +5,9 @@ from MedoTotal import *
 def depth_first_tree_search_all_count(problem, optimal=False, verbose=False):
     max_mem, visited, final_states = 0, 0, []
     frontier = [Node(problem.initial)]
+    solution = None
+    solution_cost = float('inf')
+
     while frontier:
         max_mem = max(len(frontier), max_mem)
         node = frontier.pop()
@@ -13,35 +16,35 @@ def depth_first_tree_search_all_count(problem, optimal=False, verbose=False):
             display_node(node, problem, None)
 
         expanded = node.expand(problem)
+        if node.state.tempo > 1:
+            expanded.reverse()
+
         if expanded:
-            if problem.goal_test(expanded[0].state):
-                solution = expanded[0]
-                solution, visited, final_states = search_solutions(problem, expanded, solution, visited,
-                                                                   optimal, verbose)
-                return (solution, max_mem, visited, len(final_states))
-
-        expanded.reverse()
-        frontier.extend(expanded)
-    return (None, max_mem, visited, len(final_states))
-
-
-def search_solutions(problem, expanded, solution, visited, optimal, verbose):
-    final_states = []
-    for node in expanded:
-        if node.path_cost < solution.path_cost:
-            solution = node
-        if not optimal:
-            final_states.append(node)
-            visited += 1
-            if verbose:
-                display_node(node, problem, solution)
-        elif solution not in final_states:
-            final_states.append(node)
-            visited += 1
-            if verbose:
-                display_node(node, problem, solution)
-    return solution, visited, final_states
-
+            for node in expanded:
+                if problem.goal_test(node.state):
+                    if node.path_cost < solution_cost:
+                        solution = node
+                        solution_cost = node.path_cost
+                    if not optimal:
+                        final_states.append(node)
+                        visited += 1
+                        if verbose:
+                            display_node(node, problem, solution)
+                    elif solution not in final_states:
+                        final_states.append(node)
+                        visited += 1
+                        if verbose:
+                            display_node(node, problem, solution)
+                else:
+                    if not optimal:
+                        frontier.append(node)
+                    else:
+                        if node.path_cost < solution_cost:
+                            frontier.append(node)
+    if solution:
+        return (solution, max_mem, visited, len(final_states))
+    else:
+        return (None, max_mem, visited, len(final_states))
 
 def display_node(node, problem, solution):
     print('---------------------\n')
