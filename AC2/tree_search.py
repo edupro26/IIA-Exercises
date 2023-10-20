46769e5a4c74083e42ac54d46c73d0c837e7f320
edupro26 <edupro26@gmail.com>
2023-10-20 17:58:59 +0100

    Code organization

--- AC2/tree_search.py
+++ AC2/tree_search.py
@@ -5,8 +5,7 @@ from MedoTotal import *
 def depth_first_tree_search_all_count(problem, optimal=False, verbose=False):
     max_mem, visited, final_states = 0, 0, []
     frontier = [Node(problem.initial)]
-    solution = None
-    solution_cost = float('inf')
+    solution, solution_cost = None, float('inf')
 
     while frontier:
         max_mem = max(len(frontier), max_mem)
@@ -26,25 +25,32 @@ def depth_first_tree_search_all_count(problem, optimal=False, verbose=False):
                         solution = node
                         solution_cost = node.path_cost
                     if not optimal:
-                        final_states.append(node)
-                        visited += 1
-                        if verbose:
-                            display_node(node, problem, solution)
+                        visited = save_final_state(problem, node, solution, visited, final_states, verbose)
                     elif solution not in final_states:
-                        final_states.append(node)
-                        visited += 1
-                        if verbose:
-                            display_node(node, problem, solution)
+                        visited = save_final_state(problem, node, solution, visited, final_states, verbose)
                 else:
-                    if not optimal:
-                        frontier.append(node)
-                    else:
-                        if node.path_cost < solution_cost:
-                            frontier.append(node)
+                    add_to_frontier(frontier, node, optimal, solution_cost)
     if solution:
         return (solution, max_mem, visited, len(final_states))
     else:
         return (None, max_mem, visited, len(final_states))
+
+
+def save_final_state(problem, node, solution, visited, final_states, verbose):
+    final_states.append(node)
+    visited += 1
+    if verbose:
+        display_node(node, problem, solution)
+    return visited
+
+
+def add_to_frontier(frontier, node, optimal, solution_cost):
+    if not optimal:
+        frontier.append(node)
+    else:
+        if node.path_cost < solution_cost:
+            frontier.append(node)
+
 
 def display_node(node, problem, solution):
     print('---------------------\n')
