c695a5a1d938400ca6b7291a1760d7f63a53b2ba
edupro26 <edupro26@gmail.com>
2023-10-22 18:51:46 +0100

    Code organization

--- AC2/ida_search.py
+++ AC2/ida_search.py
@@ -2,7 +2,6 @@
 from MedoTotal import *
 from GrafoAbstracto import *
 
-
 def ida_star_graph_search_count(problem, f, verbose=False):
     initial = Node(problem.initial)
     cutOff, node_count, thresholds = f(initial), 0, set()
@@ -14,13 +13,10 @@ def ida_star_graph_search_count(problem, f, verbose=False):
         node_count += 1
         while frontier:
             node = frontier.pop()
-            if verbose:
-                display(problem, f, cutOff, node)
+            display(verbose, problem, f, cutOff, node)
             if goal:
-                if verbose:
-                    display(problem, f, cutOff, None, solution)
+                display(verbose, problem, f, cutOff, None, solution)
                 return (solution, node_count)
-
             if f(node) <= cutOff:
                 child_nodes = node.expand(problem)
                 child_nodes.reverse()
@@ -30,57 +26,68 @@ def ida_star_graph_search_count(problem, f, verbose=False):
                         node_count += 1
                         visited.add(child.state)
                         if problem.goal_test(child.state):
-                            if goal:
-                                if child.path_cost <= solution.path_cost:
-                                    solution = child
-                            else:
-                                solution = child
-
-                            if f(solution) <= cutOff:
-                                goal = True
-                            else:
-                                new_cutOff.add(f(solution))
+                            goal, solution = get_best_solution(child, f, goal, solution, cutOff, new_cutOff)
                         else:
-                            frontier.append(child)
-                            if f(child) > cutOff:
-                                new_cutOff.add(f(child))
+                            add_to_frontier(frontier, f, child, cutOff, new_cutOff)
                 thresholds.update(new_cutOff)
 
         if solution:
-            if verbose:
-                display(problem, f, cutOff, None, solution)
+            display(verbose, problem, f, cutOff, None, solution)
 
-        if thresholds:
-            cutOff = min(thresholds)
-            thresholds.remove(cutOff)
-        else:
-            cutOff = float('inf')
-
+        cutOff = get_new_cutOff(thresholds)
         if cutOff == float('inf'):
             return (None, node_count)
-
         if verbose:
             print('\n')
 
 
-def display(problem, f, cutOff, node, solution=None):
-    if solution:
-        if f(solution) <= cutOff:
+def add_to_frontier(frontier, f, child, cutOff, new_cutOff):
+    frontier.append(child)
+    if f(child) > cutOff:
+        new_cutOff.add(f(child))
+
+
+def get_new_cutOff(thresholds):
+    if thresholds:
+        cutOff = min(thresholds)
+        thresholds.remove(cutOff)
+    else:
+        cutOff = float('inf')
+    return cutOff
+
+
+def get_best_solution(child, f, goal, solution, cutOff, new_cutOff):
+    if goal:
+        if child.path_cost <= solution.path_cost:
+            solution = child
+    else:
+        solution = child
+    if f(solution) <= cutOff:
+        goal = True
+    else:
+        new_cutOff.add(f(solution))
+    return goal, solution
+
+
+def display(verbose, problem, f, cutOff, node, solution=None):
+    if verbose:
+        if solution:
+            if f(solution) <= cutOff:
+                print()
+                print(problem.display(solution.state))
+                print("Cost:", solution.path_cost, "f=", f(solution))
+                print("Goal found within cutoff!")
+            else:
+                print()
+                print(problem.display(solution.state))
+                print("Cost:", solution.path_cost, "f=", f(solution))
+                print("Out of cutoff -- minimum out:", f(solution))
+        elif f(node) <= cutOff:
             print()
-            print(problem.display(solution.state))
-            print("Cost:", solution.path_cost, "f=", f(solution))
-            print("Goal found within cutoff!")
+            print(problem.display(node.state))
+            print("Cost:", node.path_cost, "f=", f(node))
         else:
             print()
-            print(problem.display(solution.state))
-            print("Cost:", solution.path_cost, "f=", f(solution))
-            print("Out of cutoff -- minimum out:", f(solution))
-    elif f(node) <= cutOff:
-        print()
-        print(problem.display(node.state))
-        print("Cost:", node.path_cost, "f=", f(node))
-    else:
-        print()
-        print(problem.display(node.state))
-        print("Cost:", node.path_cost, "f=", f(node))
-        print("Out of cutoff -- minimum out:", f(node))
+            print(problem.display(node.state))
+            print("Cost:", node.path_cost, "f=", f(node))
+            print("Out of cutoff -- minimum out:", f(node))
