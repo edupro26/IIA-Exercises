66fc2a37258d52ca73473ce1b8820437f6e5433e
edupro26 <edupro26@gmail.com>
2023-10-22 18:36:26 +0100

    Passing all tests

--- AC2/ida_search.py
+++ AC2/ida_search.py
@@ -5,41 +5,50 @@ from GrafoAbstracto import *
 
 def ida_star_graph_search_count(problem, f, verbose=False):
     initial = Node(problem.initial)
-    cutOff, node_count, thresholds = f(initial), 0, []
+    cutOff, node_count, thresholds = f(initial), 0, set()
     while True:
         if verbose:
-            print(f"------Cutoff at {cutOff}\n")
-        frontier, visited, solution = [initial], [initial.state], False
+            print(f"------Cutoff at {cutOff}")
+        goal = False
+        frontier, visited, solution = [initial], {initial.state}, None
+        node_count += 1
         while frontier:
             node = frontier.pop()
-            node_count += 1
             if verbose:
                 display(problem, f, cutOff, node)
+            if goal:
+                if verbose:
+                    display(problem, f, cutOff, None, solution)
+                return (solution, node_count)
+
             if f(node) <= cutOff:
                 child_nodes = node.expand(problem)
                 child_nodes.reverse()
+                new_cutOff = set()
                 for child in child_nodes:
                     if child.state not in visited:
+                        node_count += 1
+                        visited.add(child.state)
                         if problem.goal_test(child.state):
-                            solution = child
-                            visited.append(child.state)
-                            node_count += 1
-                            if f(child) > cutOff and f(child) not in thresholds:
-                                thresholds.append(f(child))
+                            if goal:
+                                if child.path_cost <= solution.path_cost:
+                                    solution = child
+                            else:
+                                solution = child
+
+                            if f(solution) <= cutOff:
+                                goal = True
+                            else:
+                                new_cutOff.add(f(solution))
                         else:
                             frontier.append(child)
-                            visited.append(child.state)
-            elif f(node) not in thresholds:
-                thresholds.append(f(node))
+                            if f(child) > cutOff:
+                                new_cutOff.add(f(child))
+                thresholds.update(new_cutOff)
 
         if solution:
             if verbose:
                 display(problem, f, cutOff, None, solution)
-            if f(solution) > cutOff:
-                solution = None
-        else:
-            if verbose:
-                print()
 
         if thresholds:
             cutOff = min(thresholds)
@@ -47,29 +56,31 @@ def ida_star_graph_search_count(problem, f, verbose=False):
         else:
             cutOff = float('inf')
 
-        if solution:
-            return (solution, node_count)
         if cutOff == float('inf'):
             return (None, node_count)
+
+        if verbose:
+            print('\n')
 
 
 def display(problem, f, cutOff, node, solution=None):
     if solution:
         if f(solution) <= cutOff:
+            print()
             print(problem.display(solution.state))
             print("Cost:", solution.path_cost, "f=", f(solution))
             print("Goal found within cutoff!")
         else:
+            print()
             print(problem.display(solution.state))
             print("Cost:", solution.path_cost, "f=", f(solution))
             print("Out of cutoff -- minimum out:", f(solution))
-            print('\n')
     elif f(node) <= cutOff:
+        print()
         print(problem.display(node.state))
         print("Cost:", node.path_cost, "f=", f(node))
-        print()
     else:
+        print()
         print(problem.display(node.state))
         print("Cost:", node.path_cost, "f=", f(node))
         print("Out of cutoff -- minimum out:", f(node))
-        print()
