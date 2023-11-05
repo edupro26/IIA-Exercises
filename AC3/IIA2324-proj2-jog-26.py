cfbdcbb170a6e507dfb0e51593cfdbde4ad63182
edupro26 <edupro26@gmail.com>
2023-11-05 23:25:15 +0000

    Started evaluation function development

--- AC3/IIA2324-proj2-jog-26.py
+++ AC3/IIA2324-proj2-jog-26.py
@@ -1,99 +1,51 @@
+"""
+    Avaliação Contínua 3
+
+    Grupo 26:
+    Eduardo Proença 57551
+    Alexandre Pinto 55958
+"""
+
+
 from kalah import *
 from jogos import *
 from fairKalahs import *
 from utils import *
 
-# Função para avaliar o estado do jogo
-def evaluate(state):
-    if state.is_game_over():
-        result = state.result()
-        if result == 0:
-            return 0
-        elif result == -1:
-            return -1000
+
+# ______________________________________________________________________________
+# Evaluation functions
+
+def func_26_new(state, player):
+    if not state.is_game_over():
+        p1_value, p2_value = 0, 0
+        for i in range(state.PLAY_PITS + 1):
+            p1_value += state.state[i]
+            p2_value += state.state[i + 7]
+        if player == state.SOUTH:
+            return p1_value - p2_value
         else:
-            return 1000
+            return p2_value - p1_value
+
+    winner = state.result()
+    return winner * 10 if player == state.SOUTH else winner * -10
+
+
+def func_26(estado, jogador):
+    ret = 0
+    if jogador == estado.SOUTH:
+        for i in range(6):
+            ret += estado.state[i]
+            ret -= estado.state[7 + i]
     else:
-        north_score = state.state[KalahState.NORTH_SCORE_PIT]
-        south_score = state.state[KalahState.SOUTH_SCORE_PIT]
+        for i in range(6):
+            ret -= estado.state[i]
+            ret += estado.state[7 + i]
+    if estado.is_game_over():
+        aux = estado.result()
+        return aux * 100 if jogador == estado.SOUTH else aux * -100
+    return ret
 
-        # Considere o número de sementes em cada poço para ambos os jogadores
-        north_seeds = sum(state.state[KalahState.NORTH_PITS])
-        south_seeds = sum(state.state[KalahState.SOUTH_PITS])
-
-        # Peso para o equilíbrio de sementes
-        balance_weight = 0.5
-
-        # Avaliação ponderada
-        score = (north_score - south_score) + balance_weight * (north_seeds - south_seeds)
-
-        return score
-
-
-
-
-# Função de estratégia
-def func_26(game, state):
-    def minimax(state, depth, maximizing_player):
-        if depth == 0 or game.terminal_test(state):
-            return evaluate(state)
-
-        legal_moves = game.actions(state)
-        if maximizing_player:
-            best_value = float('-inf')
-            for move in legal_moves:
-                clone_state = game.result(state, move)
-                value = minimax(clone_state, depth - 1, False)
-                best_value = max(best_value, value)
-            return best_value
-        else:
-            best_value = float('inf')
-            for move in legal_moves:
-                clone_state = game.result(state, move)
-                value = minimax(clone_state, depth - 1, True)
-                best_value = min(best_value, value)
-            return best_value
-
-    player = state.to_move
-    legal_moves = game.actions(state)
-    if not legal_moves:
-        return None  # Sem movimentos possíveis
-
-    best_move = legal_moves[0]
-    best_score = float('-inf')
-
-    for move in legal_moves:
-        clone_state = state.real_move(move)
-        score = minimax(clone_state, depth=4, maximizing_player=(player == state.to_move))
-
-        if score > best_score:
-            best_move = move
-            best_score = score
-
-    return best_move
-
-
-
-
-
-
-
-class Jogador():
-    def __init__(self, nome, fun):
-        self.nome = nome
-        self.fun = fun
-    def display(self):
-        print(self.nome+" ")
-        
-class JogadorAleat(Jogador):
-    def __init__(self, nome):
-        self.nome = nome
-        self.fun = lambda game, state: random.choice(game.actions(state))
-
-class JogadorAlfaBeta(Jogador):
-    def __init__(self, nome, depth,fun_eval):
-        self.nome = nome
-        self.fun = lambda game, state: alphabeta_cutoff_search_new(state,game,depth,eval_fn=fun_eval)
 
 def f_caos_intel(estado,jogador):
     """Quando é terminal: +100 para vitória, -100 para a derrota e 0 para o empate.
@@ -102,6 +54,35 @@ def f_caos_intel(estado,jogador):
         aux = estado.result()
         return aux*100 if jogador == estado.SOUTH else aux*-100
     return 0
+
+
+# ______________________________________________________________________________
+# Players
+
+class Jogador():
+    def __init__(self, nome, fun):
+        self.nome = nome
+        self.fun = fun
+
+    def display(self):
+        print(self.nome + " ")
+
+
+class JogadorAleat(Jogador):
+    def __init__(self, nome):
+        self.nome = nome
+        self.fun = lambda game, state: random.choice(game.actions(state))
+
+
+class JogadorAlfaBeta(Jogador):
+    def __init__(self, nome, depth, fun_eval):
+        self.nome = nome
+        self.fun = lambda game, state: alphabeta_cutoff_search_new(state, game, depth, eval_fn=fun_eval)
+
+
+# ______________________________________________________________________________
+# 1v1 function
+
 
 ##########  para ser independente dos jogos deveria devolver um método em string ou um atributo
 def joga11(game, jog1, jog2,verbose=False):
@@ -133,14 +114,9 @@ def joga11(game, jog1, jog2,verbose=False):
     return ((jog1.nome,jog2.nome),lista_jogadas,resultado)
 
 
+# ______________________________________________________________________________
+# Tournament functions
 
-##############TESTES##############
-
-
-
-el_caos = JogadorAleat("El Caos")
-func_26_player = JogadorAleat("func_26")
-el_caos_int6=JogadorAlfaBeta("El Caos Inteligente 6",6,f_caos_intel)
 
 scores={'Vitoria': 3, 'Empate': 1}
 
@@ -165,38 +141,45 @@ def jogaNpares(jogo,n,jog1,jog2):
         tabela[x]=tabelaPrim[x]+tabelaSeg[x]
     return tabelaPrim,tabelaSeg,tabela,traduzPontos(tabela)
 
+
+def incorpora(tabela, tx):
+    for jog in tx:
+        if jog not in tabela:
+            tabela[jog] = tx[jog]
+        else:
+            tabela[jog] += tx[jog]
+
+
+def torneio(n, jogadores, jogo_id=0):
+    jogo = Kalah(jogo_id)  # jogo gerado ao calha entre os "justos"
+    tabela = {}
+    for i in range(len(jogadores) - 1):
+        jog1 = jogadores[i]
+        for j in range(i + 1, len(jogadores)):
+            jog2 = jogadores[j]
+            _, _, _, tabelaX = jogaNpares(jogo, n, jog1, jog2)
+            incorpora(tabela, tabelaX)
+    return tabela
+
+
+# ______________________________________________________________________________
+# TESTS
+
+el_caos_int3=JogadorAlfaBeta("El Caos Inteligente 3",3,f_caos_intel)
+el_caos_int6=JogadorAlfaBeta("El Caos Inteligente 6",6,f_caos_intel)
+el_caos_int1=JogadorAlfaBeta("El Caos Inteligente 1",2,f_caos_intel)
+jogador_26=JogadorAlfaBeta("Jogador26", 2, func_26)
+jogador_26new=JogadorAlfaBeta("Jogador26new", 2, func_26_new)
 jogo=Kalah(10)
-result = jogaNpares(jogo, 100, el_caos_int6, func_26_player)
-print(result)
 
-# Crie uma instância de Kalah para jogar
-#game = Kalah(20)
+#_,_,res = joga11(jogo, jogador26, el_caos_int6)
+#print(res)
 
-# Crie instâncias dos jogadores
-#el_caos = JogadorAleat("El Caos")
-#func_26_player = JogadorAleat("func_26")
+#_,_,res,s = jogaNpares(jogo,10, jogador26, el_caos_int6)
+#print(res,s)
 
-# Realize um jogo entre os jogadores
-#_, _, resultado = joga11(game, func_26_player, el_caos)
+#res = torneio(50,[jogador26,el_caos_int6,el_caos_int3],10)
+#print(res)
 
-# Exiba o resultado do jogo
-#print("Resultado:", resultado)
-
-
-
-
-# Se desejar, pode criar mais funções auxiliares ou testes aqui
-
-# Exemplo de uso
-#if __name__ == "__main__":
-#    game = Kalah()
-#    state = game.initial
-#    game.display(state)
-#    while not game.terminal_test(state):
-#        move = func_26(game, state)
-#        if move is not None:
-#            state = game.result(state, move)
-#            game.display(state)
-#        else:
-#            print("Nenhum movimento disponível. O jogo terminou.")
-#            break
+res = torneio(100,[jogador_26,jogador_26new,el_caos_int1],100)
+print(res)
