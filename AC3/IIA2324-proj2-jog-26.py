71881779945646ec955bfc512916fee2d5ff8f55
Eduardo Proença <edupro26@gmail.com>
2023-11-06 11:30:02 +0000

    Trying some new conditions in func_26_new

--- AC3/IIA2324-proj2-jog-26.py
+++ AC3/IIA2324-proj2-jog-26.py
@@ -17,21 +17,63 @@ from utils import *
 # Evaluation functions
 
 def func_26_new(state, player):
-    if not state.is_game_over():
-        p1_value, p2_value = 0, 0
-        for i in range(state.PLAY_PITS + 1):
-            p1_value += state.state[i]
-            p2_value += state.state[i + 7]
-        if player == state.SOUTH:
-            return p1_value - p2_value
+    if state.is_game_over():
+        winner = state.result()
+        return winner * 100 if player == state.SOUTH else winner * -100
+
+    south_sum, north_sum = 0, 0
+    for i in range(state.PLAY_PITS+1):
+        south_sum += state.state[i]
+        north_sum += state.state[i+7]
+
+    if player == state.SOUTH:
+        value = 0
+        score_pit = state.SOUTH_SCORE_PIT
+        for i in range(state.PLAY_PITS):
+            if score_pit - i == state.state[i]:
+                value += 10
+
+        if south_sum >= north_sum:
+            return (south_sum - north_sum) + value
         else:
-            return p2_value - p1_value
+            return (south_sum - north_sum) - value
+        #value += (south_sum - north_sum)
+    else:
+        value = 0
+        score_pit = state.NORTH_SCORE_PIT
+        for i in range(state.PLAY_PITS):
+            if score_pit - (i+7) == state.state[i+7]:
+                value += 10
 
-    winner = state.result()
-    return winner * 10 if player == state.SOUTH else winner * -10
+        if north_sum >= south_sum:
+            return (north_sum - south_sum) + value
+        else:
+            return (north_sum - south_sum) - value
+        #value += (north_sum - south_sum)
+
+    #return value
 
 
-def func_26(estado, jogador):
+def func_26(state, player):
+    if state.is_game_over():
+        winner = state.result()
+        return winner * 100 if player == state.SOUTH else winner * -100
+
+    south_sum, north_sum = 0, 0
+    for i in range(state.PLAY_PITS+1):
+        south_sum += state.state[i]
+        north_sum += state.state[i+7]
+
+    if player == state.SOUTH:
+        value = south_sum - north_sum
+    else:
+        value = north_sum - south_sum
+
+    return value
+
+
+# This code might not be the exact chapiteau code
+def chapiteau_replica(estado, jogador):
     ret = 0
     if jogador == estado.SOUTH:
         for i in range(6):
@@ -167,7 +209,8 @@ def torneio(n, jogadores, jogo_id=0):
 
 el_caos_int3=JogadorAlfaBeta("El Caos Inteligente 3",3,f_caos_intel)
 el_caos_int6=JogadorAlfaBeta("El Caos Inteligente 6",6,f_caos_intel)
-el_caos_int1=JogadorAlfaBeta("El Caos Inteligente 1",2,f_caos_intel)
+el_caos_int2=JogadorAlfaBeta("El Caos Inteligente 2",2,f_caos_intel)
+chapiteau = JogadorAlfaBeta("Chapiteau", 2, chapiteau_replica)
 jogador_26=JogadorAlfaBeta("Jogador26", 2, func_26)
 jogador_26new=JogadorAlfaBeta("Jogador26new", 2, func_26_new)
 jogo=Kalah(10)
@@ -181,5 +224,5 @@ jogo=Kalah(10)
 #res = torneio(50,[jogador26,el_caos_int6,el_caos_int3],10)
 #print(res)
 
-res = torneio(100,[jogador_26,jogador_26new,el_caos_int1],100)
+res = torneio(150,[jogador_26,jogador_26new,chapiteau],20)
 print(res)
