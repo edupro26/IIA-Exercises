46e2c7a0c4014f5330d74db5d8f6e2e5d0b41af8
edupro26 <edupro26@gmail.com>
2023-11-13 19:41:24 +0000

    Finished evaluation function

--- AC3/evaluation_function.py
+++ AC3/evaluation_function.py
@@ -1,5 +1,5 @@
 """
-    Avaliação Contínua 3
+    Avaliação Contínua 3 - Kalah
 
     Grupo 26:
     Eduardo Proença 57551
@@ -10,11 +10,10 @@ from tournament import *
 
 
 # ______________________________________________________________________________
-# Evaluation functions
+# Evaluation function
 
-# https://digitalcommons.andrews.edu/cgi/viewcontent.cgi?article=1259&context=honors
 
-def func_26_v4(state, player):
+def func_26(state, player):
     if state.is_game_over():
         result = state.result()
         return result * 100 if player == state.SOUTH else result * -100
@@ -24,29 +23,55 @@ def func_26_v4(state, player):
     h3 = possibleExtraMoves(state, player)
     h4 = possibleSteals(state, player)
     h5 = winningProbability(state, player)
-    h6 = opponentEmptyPits(state, player)
+    h6 = emptyPits(state, player)
+    h7 = maxPlayerScore(state, player)
+    h8 = minOpponentScore(state, player)
 
     total_pieces = state.NUM_PIECES
     south_score = state.state[state.SOUTH_SCORE_PIT]
     north_score = state.state[state.NORTH_SCORE_PIT]
     if total_pieces - (south_score + north_score) < 10:
-        weights = [0.8, 0.5, 0.8, 0.4, 0.3, 0.4]  # More tunnig needed
+        weights = [1.12, 0.32, 0.68, 0.54, 0.62, 0.22, 0.66, 0.66]
     else:
-        weights = [0.8, 0.1, 0.4, 0.2, 0.6, 0.2]  # More tunnig needed
-    value = sum(h * w for h, w in zip([h1, h2, h3, h4, h5, h6], weights))
+        weights = [0.82, 0.08, 0.48, 0.34, 0.62, 0.04, 0.66, 0.66]
+    value = sum(h * w for h, w in zip([h1, h2, h3, h4, h5, h6, h7, h8], weights))
     return value
 
 
-def opponentEmptyPits(state, player):
-    count = 0
+# ______________________________________________________________________________
+# Heuristics
+
+
+def minOpponentScore(state, player):
+    """ Attempts to minimize the opponent score """
+    score = state.state[state.NORTH_SCORE_PIT] if player == state.SOUTH \
+        else state.state[state.SOUTH_SCORE_PIT]
+    return -score
+
+
+def maxPlayerScore(state, player):
+    """ Attempts to maximize the opponent score """
+    score = state.state[state.SOUTH_SCORE_PIT] if player == state.SOUTH \
+        else state.state[state.NORTH_SCORE_PIT]
+    return score
+
+
+def emptyPits(state, player):
+    """ Compares the number of empty pits from the opponent
+    side to the number of empty pits from the player side """
+    playerEP, opponentEP = 0, 0
     for i in range(state.PLAY_PITS):
-        pit = i if player == state.NORTH else i + state.PLAY_PITS + 1
-        if state.state[pit] == 0:
-            count += 1
-    return count
+        player_pit = i if state.SOUTH else i + state.PLAY_PITS + 1
+        opponent_pit = i + state.PLAY_PITS + 1 if player == state.SOUTH else i
+        if state.state[player_pit] == 0:
+            playerEP += 1
+        if state.state[opponent_pit] == 0:
+            opponentEP += 1
+    return opponentEP - playerEP
 
 
 def winningProbability(state, player):
+    """ Evaluates the player's winning probability """
     value = 0
     south_score = state.state[state.SOUTH_SCORE_PIT]
     north_score = state.state[state.NORTH_SCORE_PIT]
@@ -58,6 +83,7 @@ def winningProbability(state, player):
 
 
 def possibleSteals(state, player):
+    """ Counts possible pieces steals """
     count = 0
     for i in range(1, state.PLAY_PITS):
         player_pit = i if player == state.SOUTH \
@@ -70,6 +96,7 @@ def possibleSteals(state, player):
 
 
 def possibleExtraMoves(state, player):
+    """ Counts possible extra moves """
     count = 0
     score_pit = state.SOUTH_SCORE_PIT if player == state.SOUTH \
         else state.NORTH_SCORE_PIT
@@ -81,6 +108,8 @@ def possibleExtraMoves(state, player):
 
 
 def comparePieces(state, player):
+    """ Compares the pieces in the player side to
+    the pieces in the opponent side """
     south_pieces, north_pieces = 0, 0
     for i in range(state.PLAY_PITS):
         south_pieces += state.state[i] * 0.25
@@ -90,58 +119,25 @@ def comparePieces(state, player):
 
 
 def compareScores(state, player):
+    """ Compares the player score with the opponent's """
     south_score = state.state[state.SOUTH_SCORE_PIT]
     north_score = state.state[state.NORTH_SCORE_PIT]
     return south_score - north_score if player == state.SOUTH \
         else north_score - south_score
 
 
-def func_26_v3(state, player):
-    value = 0
-    if state.is_game_over():
-        result = state.result()
-        return result*100 if player == state.SOUTH else result*-100
-
-    south_score = state.state[state.SOUTH_SCORE_PIT]
-    north_score = state.state[state.NORTH_SCORE_PIT]
-    if player == state.SOUTH:
-        score_pit = state.SOUTH_SCORE_PIT
-        for i in range(state.PLAY_PITS):
-            if score_pit - i == state.state[i]:
-                value += 1
-        value += (south_score - north_score)
-    else:
-        score_pit = state.NORTH_SCORE_PIT
-        for i in range(state.PLAY_PITS):
-            if score_pit - (i+7) == state.state[i+7]:
-                value += 1
-        value += (north_score - south_score)
-
-    return value
-
-
 # ______________________________________________________________________________
 # TESTS
 
-aleatorio = JogadorAleat("Aleatório")
-el_caos_int = JogadorAlfaBeta("El Caos Inteligente", 3, f_caos_intel)
-chapiteau = JogadorAlfaBeta("Chapiteau", 3, chapiteau_replica)
-jogador_26_v3 = JogadorAlfaBeta("Jogador26_v3", 3, func_26_v3)
-jogador_26_v4 = JogadorAlfaBeta("Jogador26_v4", 3, func_26_v4)
+chapiteau = JogadorAlfaBeta("chapiteau", 4, chapiteau_replica)
+jogador_26 = JogadorAlfaBeta("jogador_26", 4, func_26)
 
 win_rates = []
-player1, player2 = 0, 0
-for i in range(7):
-    res = torneio(50, [jogador_26_v3, jogador_26_v4], 10)
-    print(res)
-    player1 += res["Jogador26_v3"]
-    player2 += res["Jogador26_v4"]
-    win_rates.append(player2 / player1)
+jogo=Kalah(0)
+for i in range(3):
+    _, _, res, p = jogaNpares(jogo, 100, chapiteau, jogador_26)
+    print("Games:", res)
+    print("Points:", p)
+    print("----------------------------------------------------------")
+    win_rates.append(res["jogador_26"] / res["chapiteau"])
 print("Win rate average:", sum(win_rates)/len(win_rates))
-
-#res = torneio(200, [chapiteau, jogador_26_v3, jogador_26_v4], 100)
-#print(res)
-
-#jogo = Kalah(0)
-#_, _, res = joga11(jogo, jogador_26_v3, jogador_26_v4, True)
-#print(res)
