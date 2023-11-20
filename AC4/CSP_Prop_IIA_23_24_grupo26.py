3cd43a85b8835a396ab72149935972eb0f8d9608
Eduardo Proença <edupro26@gmail.com>
2023-11-20 11:26:24 +0000

    Variables done. Started domains

--- AC4/CSP_Prop_IIA_23_24_grupo26.py
+++ AC4/CSP_Prop_IIA_23_24_grupo26.py
@@ -11,20 +11,42 @@ from csp import *
 
 def csp_prop(formulas):
     variables = []
+    domains = dict()
+    neighbors = None
+    constraints = None
+
+    # Get variables and domains
     for i in formulas:
         symbols = prop_symbols(i)
         for j in symbols:
+            possible_values = [False, True]
             if j.op not in variables:
                 variables.append(j.op)
+                variables.sort()
+                if is_prop_symbol(i.op):
+                    domains[j.op] = True # Not working needs fixing
+                else:
+                    domains[j.op] = possible_values
 
-    variables.sort()
-    csp = CSP(variables, None, None, None)
+    csp = CSP(variables, domains, neighbors, constraints)
     return csp
 
 
+# ___________________________________________________________________________________
+# TESTS
+
+# 1.
 try:
     formulas={expr('A ==> (B & C)'),expr('A')}
     abc_csp=csp_prop(formulas)
     print(abc_csp.variables)
+except Exception as e:
+    print(repr(e))
+
+# 2.
+try:
+    formulas={expr('A ==> (B & C)'),expr('A')}
+    abc_csp=csp_prop(formulas)
+    print(sorted([(var,sorted(val)) for (var,val) in abc_csp.domains.items()]))
 except Exception as e:
     print(repr(e))
