a7bc059bb7224d100a2c736a0c9bf38443347a8b
edupro26 <edupro26@gmail.com>
2023-11-24 17:10:57 +0000

    Finished AC4

--- AC4/CSP_Prop_IIA_23_24_grupo26.py
+++ AC4/CSP_Prop_IIA_23_24_grupo26.py
@@ -10,17 +10,6 @@ from logic import *
 from csp import *
 
 def csp_prop(formulas):
-    # Get variables from formulas
-    variables = get_variables(formulas)
-
-    # Get domains from formulas
-    domains = get_domains(formulas, variables)
-
-    # Convert formulas to cnf
-    forms_cnf = convert_to_cnf(formulas)
-
-    # Get neighbors from formulas
-    neighbors = get_neighbors(forms_cnf, variables)
 
     def constraints(A, a, B, b):
         eval = True
@@ -35,6 +24,12 @@ def csp_prop(formulas):
                 elif len(vars) == 1 and B in vars:
                     eval = pl_true(form, {expr(B): model[B]})
         return eval
+
+    # Get variables, domains and neighbors
+    variables = get_variables(formulas)
+    domains = get_domains(formulas, variables)
+    forms_cnf = convert_to_cnf(formulas)
+    neighbors = get_neighbors(forms_cnf, variables)
 
     return CSP(variables, domains, neighbors, constraints)
 
@@ -75,58 +70,3 @@ def get_variables(formulas):
         symbols = prop_symbols(form)
         variables.update([j.op for j in symbols])
     return sorted(variables)
-
-
-# ___________________________________________________________________________________
-# TESTS
-
-# 1.
-try:
-    formulas={expr('A ==> (B & C)'),expr('A')}
-    abc_csp=csp_prop(formulas)
-    print(abc_csp.variables)
-except Exception as e:
-    print(repr(e))
-
-# 2.
-try:
-    formulas={expr('A ==> (B & C)'),expr('A')}
-    abc_csp=csp_prop(formulas)
-    print(sorted([(var,sorted(val)) for (var,val) in abc_csp.domains.items()]))
-except Exception as e:
-    print(repr(e))
-
-# 3.
-try:
-    formulas={expr('A ==> (B & C)'),expr('A')}
-    abc_csp=csp_prop(formulas)
-    print(sorted([(var,sorted(val)) for (var,val) in abc_csp.neighbors.items()]))
-except Exception as e:
-    print(repr(e))
-
-# 4.
-try:
-    formulas={expr('A & ~A')}
-    a_csp=csp_prop(formulas)
-    print(sorted([(var,sorted(val)) for (var,val) in a_csp.neighbors.items()]))
-except Exception as e:
-    print(repr(e))
-
-# 5.
-try:
-    formulas={expr('A ==> (B & C)'),expr('A')}
-    abc_csp=csp_prop(formulas)
-    r = backtracking_search(abc_csp)
-    print('Assignment = ',r)
-except Exception as e:
-    print(repr(e))
-
-# 6.
-try:
-    formulas={expr('VA | VP'),expr('AV | AP'),expr('PV | PA'),expr('VP'),expr('VA ==> ~VP'),expr('AP ==> ~AV'),
-              expr('PA ==> ~PV'),expr('VA ==> ~PA'),expr('PV ==> ~AV'),expr('VP ==> ~AP')}
-    dancam_csp=csp_prop(formulas)
-    r = backtracking_search(dancam_csp)
-    print('Assignment = ',r)
-except Exception as e:
-    print(repr(e))
