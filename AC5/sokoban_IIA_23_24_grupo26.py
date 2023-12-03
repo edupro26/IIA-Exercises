6ef3f9a214767c6f8a310c49fc82917129a13e13
edupro26 <edupro26@gmail.com>
2023-12-03 18:37:21 +0000

    Started with ChatGPT code

--- AC5/sokoban_IIA_23_24_grupo26.py
+++ AC5/sokoban_IIA_23_24_grupo26.py
@@ -14,7 +14,58 @@ from search import *
 
 
 def sokoban(puzzle):
-    pass
+    """
+    Creates a ForwardPlan instance for the Sokoban problem.
+
+    Parameters:
+    - puzzle: A string representing the Sokoban puzzle.
+
+    Returns:
+    - forward_plan: A ForwardPlan instance.
+    """
+    def process_puzzle(puzzle):
+        """
+        Process the puzzle string and extract initial state and goals.
+
+        Returns:
+        - initial_state: List of Expr representing the initial state.
+        - goals: List of Expr representing the goals.
+        """
+        lines = puzzle.strip().split('\n')
+        initial_state = []
+        goals = []
+        for row, line in enumerate(lines):
+            for col, char in enumerate(line):
+                if char == '#':
+                    # Wall
+                    initial_state.append(Expr('Wall', row, col))
+                elif char == '.':
+                    # Empty location
+                    initial_state.append(Expr('Empty', row, col))
+                elif char == 'o':
+                    # Storage location
+                    goals.append(Expr('Storage', row, col))
+                elif char == '@':
+                    # Sokoban player
+                    initial_state.append(Expr('Player', row, col))
+                elif char == '$':
+                    # Box
+                    initial_state.append(Expr('Box', row, col))
+                elif char == '+':
+                    # Sokoban player on storage
+                    initial_state.append(Expr('Player', row, col))
+                    goals.append(Expr('Storage', row, col))
+        return initial_state, goals
+
+    # Process the puzzle string
+    initial_state, goals = process_puzzle(puzzle)
+
+    planning = PlanningProblem(initial_state, goals, [], [])
+
+    # Create a ForwardPlan instance
+    forward_plan = ForwardPlan(planning)
+
+    return forward_plan
 
 
 # ____________________________________________________________________
